e8643a31c0dc2d19e733ef374bf8ce70d6a4ccfc
Lucas <lucaasferreirafl@gmail.com>
2022-06-19 22:12:15 -0300

    feat: initial setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Header } from './components/Header'
+import { Post } from './Post'
+
+import './styles.css'
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <Post 
+        author="Lucas Ferreira"
+        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid maiores numquam consequatur velit deleniti inventore, recusandae nisi laudantium quaerat enim quasi quisquam dolorum, sequi natus obcaecati possimus quas, minus molestiae!" 
+      />
+      <Post 
+        author="Wagner Junio"
+        content="New post!" 
+      />
+    </div>
+  )
+}
