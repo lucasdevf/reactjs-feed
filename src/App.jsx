2226eb39b31e9213334cfd881430997ba020d3f3
Lucas <lucaasferreirafl@gmail.com>
2022-06-20 00:40:16 -0300

    feat: dynamic data in post component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,36 @@ import styles from './App.module.css'
 
 import './global.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/lucasdevf.png',
+      name: 'Lucas Ferreira',
+      role: 'Tech Lead at GHAP Tecnologia'
+    },
+    content: [
+      { type: 'paragraph', content: 'Falaaa dev!' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2022-06-20 22:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Guilherme Gonzaga',
+      role: 'CEO at GHAP Tecnologia'
+    },
+    content: [
+      { type: 'paragraph', content: 'Falaaa GHAP!' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2022-06-20 23:00:00')
+  }
+]
 
 export function App() {
   return (
@@ -17,14 +47,7 @@ export function App() {
         <Sidebar />
 
         <main>
-        <Post 
-          author="Lucas Ferreira"
-          content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid maiores numquam consequatur velit deleniti inventore, recusandae nisi laudantium quaerat enim quasi quisquam dolorum, sequi natus obcaecati possimus quas, minus molestiae!" 
-        />
-        <Post 
-          author="Wagner Junio"
-          content="New post!" 
-        />
+          {posts.map(post => <Post author={post.author} content={post.content} publishedAt={post.publishedAt} />)}
         </main>
 
       </div>
