dc3d07c577aeb7088f493d0f970fe163ab54ad2d
Lucas <lucaasferreirafl@gmail.com>
2022-06-20 21:30:01 -0300

    feat: disabled button when field new comment is empty

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -32,7 +32,12 @@ export function Post({ author, publishedAt, content }) {
   }
 
   function handleNewCommentChange(e) {
+    e.target.setCustomValidity('')
     setNewCommentText(e.target.value)
+  }
+
+  function handleNewCommentInvalid(e) {
+    e.target.setCustomValidity('Este campo é obrigatório.')
   }
 
   function deleteComment(commentToDelete) {
@@ -40,6 +45,8 @@ export function Post({ author, publishedAt, content }) {
 
     setComments(commentsWithoutDeletedOne)
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0
 
   return (
     <article className={styles.post}>
@@ -77,10 +84,17 @@ export function Post({ author, publishedAt, content }) {
           placeholder="Deixe um comentário"
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type="submit">Publicar</button>
+          <button 
+            type="submit" 
+            disabled={isNewCommentEmpty}
+          >
+            Publicar
+          </button>
         </footer>
       </form>
 
