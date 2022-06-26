96d76b14bf3b3deba9c816595f3cf60c5045dac8
Lucas <lucaasferreirafl@gmail.com>
2022-06-26 09:38:55 -0300

    feat: implements typescript

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
+import { Post } from './components/Post'
+
+import styles from './App.module.css'
+
+import './global.css'
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/lucasdevf.png',
+      name: 'Lucas Ferreira',
+      role: 'Tech Lead at GHAP Tecnologia'
+    },
+    content: [
+      { type: 'paragraph', content: 'Falaaa dev!' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2022-06-20 22:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Guilherme Gonzaga',
+      role: 'CEO at GHAP Tecnologia'
+    },
+    content: [
+      { type: 'paragraph', content: 'Falaaa GHAP!' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2022-06-20 23:00:00')
+  }
+]
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+
+        <Sidebar />
+
+        <main>
+          {posts.map(post => <Post key={post.id} author={post.author} content={post.content} publishedAt={post.publishedAt} />)}
+        </main>
+
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Comment.tsx
@@ -0,0 +1,57 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import { useState } from 'react'
+import { Avatar } from './Avatar'
+import styles from './Comment.module.css'
+
+interface CommentProps {
+  content: string
+  onDeleteComment: (comment: string) => void
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+
+  const [likeCount, setLikeCount] = useState(0)
+
+  function handleDeleteComment(){ 
+    onDeleteComment(content)
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1)
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/lucasdevf.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndtime}>
+              <strong>Lucas Ferreira</strong>
+              <time 
+                title="19 de Junho às 22:00" 
+                dateTime="2022-06-19 22:00"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button onClick={handleDeleteComment} title="Excluir comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
